07a01d67bcc324e8b6f4cebb060262d3cf5fc4bc
hhjk ccff <abddaniil@yandex.ru>
2026-09-06 13:38:30 +0300

    library update

--- shared/src/commonMain/kotlin/org/unstabledev/pomegranate/P2PUtils/P2PManagerImpl.kt
+++ shared/src/commonMain/kotlin/org/unstabledev/pomegranate/P2PUtils/P2PManagerImpl.kt
@@ -3,8 +3,7 @@ package org.unstabledev.pomegranate.P2PUtils
 expect class P2PManagerImpl(tempDir : String) {
     var channel: P2PChannelImpl?
     suspend fun fork(): P2PManagerImpl
-    suspend fun getAddress(): String?
-    suspend fun getLocalAddress(): String?
-    suspend fun createConnection(remoteAddress: String, remoteLocalAddress: String): P2PChannelImpl
+    fun getICECandidates(): String
+    suspend fun createConnection(iceCandidates: String): P2PChannelImpl
     fun breakConnection()
 }
